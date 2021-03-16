b8cf50a0b0cf755bb3b5fc685be3122014893426
Anand U <Anand@gmail.com>
2021-03-16 13:18:07 +0000

    code intended changes

--- src/LockedMe_Application/DeleteFile.java
+++ src/LockedMe_Application/DeleteFile.java
@@ -3,27 +3,27 @@ package LockedMe_Application;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class DeleteFile {
-	
+public class DeleteFile 
+	{
+
 	static String path = "/home/anandudell/eclipse-workspace/LockedMe.com/";
 
 	public static void main(String[] args) {
 		deletefile(path);
-		
+
 	}
 	public static void deletefile(String path) {
 		try {
-			
+
 			Files.delete(Paths.get(path));
 			System.out.println("File is deleted successfully");
-			
+
 		} catch (Exception e) {
 			System.out.println("File does not exist!");
 		}
 	}
 }
 
-		
-			
-		
-	
+
+
+
--- src/LockedMe_Application/LockmeApp.java
+++ src/LockedMe_Application/LockmeApp.java
@@ -1,55 +1,57 @@
 package LockedMe_Application;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
-
-import javax.swing.tree.DefaultTreeCellEditor.DefaultTextField;
 
 public class LockmeApp {
 
 	public static void main(String[] args) {
 		// Display Welcome Screen
-		
-		System.out.println("Welcome to LockMe Application");
-		System.out.println("Developer: U_Anand");
-		System.out.println("Please select any of the below Options: 1. Add a File  " + "2.Delete a File" + "3. Search for an File");
-		Scanner inputs=new Scanner(System.in);
-		int Userinput =inputs.nextInt();
-		if(Userinput<=3){
-			System.out.println("User Selected option is :: " +Userinput);
-			
-		}
-		else
-		{
-		System.out.println("User selected an invalid option");
-		}	
-		
-		CreateFile createfl=new CreateFile();
-		DeleteFile deletefl=new DeleteFile();
-		SearchFile sfile=new SearchFile();
-		if(Userinput==1) {
-			System.out.println("Enter the File name to be added::");
-			Scanner Filetoadd=new Scanner(System.in);
-			String Addfile=Filetoadd.next();
-			createfl.createfile(Addfile);
+
+		try {
+
+			System.out.println("--------------*Welcome to LockMe Application*-------------");
+			System.out.println("Developer: Anand_U");
+			System.out.println("Please select any of the below Options \n 1. Add a File  " + "2.Delete a File" +  " 3. Search a File");
+			Scanner inputs=new Scanner(System.in);
+			int Userinput =inputs.nextInt();
+			if(Userinput<=3){
+				System.out.println("User Selected option is :: " +Userinput);
+
+			}
+			else
+			{
+				System.out.println("User selected an invalid option");
+			}	
+
+			CreateFile createfl=new CreateFile();
+			DeleteFile deletefl=new DeleteFile();
+			SearchFile sfile=new SearchFile();
+			if(Userinput==1) {
+				System.out.println("Enter the File name to be added::");
+				Scanner Filetoadd=new Scanner(System.in);
+				String Addfile=Filetoadd.next();
+				createfl.createfile(Addfile);
 			}
 			else if (Userinput==2){
 				System.out.println("Enter the file name to be deleted::");
 				Scanner Filetodelete=new Scanner(System.in);
 				String Delfile =Filetodelete.next();
 				deletefl.deletefile(Delfile);
-				
+
 			}
-			else {
+			else if(Userinput==3)
 				System.out.println("Enter Filename to search for::");
-				Scanner Filetosearch=new Scanner(System.in);
-				String Searchfile =Filetosearch.next();
-				sfile.searcfile(Searchfile);
-				
-			}
-		
-		
+			Scanner Filetosearch=new Scanner(System.in);
+			String Searchfile =Filetosearch.next();
+			sfile.searcfile(Searchfile);
+
+
+		} catch (Exception e) {
+			System.out.println("User Entered Invalid option::");
+			System.exit(1);
+		}
+
+
 	}
 
 	private static String DeleteFile() {
